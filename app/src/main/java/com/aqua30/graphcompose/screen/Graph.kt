fe95ba5efc5d0a812d6c834d41b12020e63a4749
soo <onemask@naver.com>
2023-02-08 11:34:06 +0900

    line 정리

--- app/src/main/java/com/aqua30/graphcompose/screen/Graph.kt
+++ app/src/main/java/com/aqua30/graphcompose/screen/Graph.kt
@@ -24,109 +24,146 @@ import androidx.compose.ui.unit.sp
  */
 @Composable
 fun Graph(
-    modifier : Modifier,
-    xValues: List<Int>,
-    yValues: List<Int>,
-    points: List<Float>,
-    paddingSpace: Dp,
-    verticalStep: Int
+  modifier: Modifier,
+  xValues: List<Int>,
+  yValues: List<Int>,
+  points: List<Float>,
+  paddingSpace: Dp,
+  verticalStep: Int,
 ) {
-    val controlPoints1 = mutableListOf<PointF>()
-    val controlPoints2 = mutableListOf<PointF>()
-    val coordinates = mutableListOf<PointF>()
-    val density = LocalDensity.current
-    val textPaint = remember(density) {
-        Paint().apply {
-            color = android.graphics.Color.BLACK
-            textAlign = Paint.Align.CENTER
-            textSize = density.run { 12.sp.toPx() }
-        }
+  val controlPoints2 = mutableListOf<PointF>()
+  val coordinates = mutableListOf<PointF>()
+  val density = LocalDensity.current
+  val textPaint = remember(density) {
+    Paint().apply {
+      color = android.graphics.Color.BLACK
+      textAlign = Paint.Align.CENTER
+      textSize = density.run { 12.sp.toPx() }
     }
+  }
 
-    Box(
-        modifier = modifier
-            .background(Color.White)
-            .padding(horizontal = 8.dp, vertical = 12.dp),
-        contentAlignment = Center
+  Box(
+    modifier = modifier
+      .background(Color.White)
+      .padding(horizontal = 8.dp, vertical = 12.dp),
+    contentAlignment = Center
+  ) {
+    Canvas(
+      modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Gray),
     ) {
-        Canvas(
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            val xAxisSpace = (size.width - paddingSpace.toPx()) / xValues.size
-            val yAxisSpace = size.height / yValues.size
-            /** placing x axis points */
-            for (i in xValues.indices) {
-                drawContext.canvas.nativeCanvas.drawText(
-                    "${xValues[i]}",
-                    xAxisSpace * (i + 1),
-                    size.height - 30,
-                    textPaint
-                )
-            }
-            /** placing y axis points */
-            for (i in yValues.indices) {
-                drawContext.canvas.nativeCanvas.drawText(
-                    "${yValues[i]}",
-                    paddingSpace.toPx() / 2f,
-                    size.height - yAxisSpace * (i + 1),
-                    textPaint
-                )
-            }
-            /** placing our x axis points */
-            for (i in points.indices) {
-                val x1 = xAxisSpace * xValues[i]
-                val y1 = size.height - (yAxisSpace * (points[i]/verticalStep.toFloat()))
-                coordinates.add(PointF(x1,y1))
-                /** drawing circles to indicate all the points */
-                drawCircle(
-                    color = Color.Red,
-                    radius = 10f,
-                    center = Offset(x1,y1)
-                )
-            }
-            /** calculating the connection points */
-            for (i in 1 until coordinates.size) {
-                controlPoints1.add(PointF((coordinates[i].x + coordinates[i - 1].x) / 2, coordinates[i - 1].y))
-                controlPoints2.add(PointF((coordinates[i].x + coordinates[i - 1].x) / 2, coordinates[i].y))
-            }
-            /** drawing the path */
-            val stroke = Path().apply {
-                reset()
-                moveTo(coordinates.first().x, coordinates.first().y)
-                for (i in 0 until coordinates.size - 1) {
-                    cubicTo(
-                        controlPoints1[i].x,controlPoints1[i].y,
-                        controlPoints2[i].x,controlPoints2[i].y,
-                        coordinates[i + 1].x,coordinates[i + 1].y
-                    )
-                }
-            }
-            /** filling the area under the path */
-            val fillPath = android.graphics.Path(stroke.asAndroidPath())
-                .asComposePath()
-                .apply {
-                    lineTo(xAxisSpace * xValues.last(), size.height - yAxisSpace)
-                    lineTo(xAxisSpace, size.height - yAxisSpace)
-                    close()
-                }
-            drawPath(
-                fillPath,
-                brush = Brush.verticalGradient(
-                    listOf(
-                        Color.Cyan,
-                        Color.Transparent,
-                    ),
-                    endY = size.height - yAxisSpace
-                ),
-            )
-            drawPath(
-                stroke,
-                color = Color.Black,
-                style = Stroke(
-                    width = 5f,
-                    cap = StrokeCap.Round
-                )
-            )
+      val xAxisSpace = (size.width - paddingSpace.toPx()) / xValues.size
+      val yAxisSpace = size.height / yValues.size
+      /** placing x axis points */
+      for (i in xValues.indices) {
+        drawContext.canvas.nativeCanvas.drawText(
+          "${xValues[i]}",
+          xAxisSpace * (i + 1),
+          size.height - 30,
+          textPaint
+        )
+      }
+      /** placing y axis points */
+      for (i in yValues.indices) {
+        drawContext.canvas.nativeCanvas.drawText(
+          "${yValues[i]}",
+          paddingSpace.toPx() / 2f,
+          size.height - yAxisSpace * (i + 1),
+          textPaint
+        )
+      }
+      /** placing our x axis points */
+      for (i in points.indices) {
+        val x1 = xAxisSpace * xValues[i]
+        val y1 = size.height - (yAxisSpace * (points[i] / verticalStep.toFloat()))
+        coordinates.add(
+          PointF(x1, y1)
+        )
+        /** drawing circles to indicate all the points */
+        drawCircle(color = Color.Red, radius = 10f, center = Offset(x1, y1))
+      }
+
+      /** calculating the connection points */
+      val controlPoints1 = mutableListOf<PointF>()
+      for (i in 1 until coordinates.size) {
+        controlPoints1.add(
+          PointF(
+            (coordinates[i].x + coordinates[i - 1].x) / 2,
+            coordinates[i - 1].y
+          )
+        )
+        controlPoints2.add(
+          PointF(
+            (coordinates[i].x + coordinates[i - 1].x) / 2,
+            coordinates[i].y
+          )
+        )
+      }
+      /** drawing the path */
+      val stroke: Path = Path().apply {
+        reset()
+        moveTo(coordinates.first().x, coordinates.first().y)
+        for (i in 0 until coordinates.size - 1) {
+          cubicTo(controlPoints1[i].x,
+            controlPoints1[i].y,
+            controlPoints2[i].x,
+            controlPoints2[i].y,
+            coordinates[i + 1].x,
+            coordinates[i + 1].y)
         }
+      }
+
+      /** filling the area under the path */
+      val fillPath: Path = android.graphics.Path(stroke.asAndroidPath()).asComposePath().apply {
+        lineTo(xAxisSpace * xValues.last(), size.height - yAxisSpace)
+        lineTo(xAxisSpace, size.height - yAxisSpace)
+        close()
+      }
+
+      val dottedStroke: Path = Path().apply {
+        reset()
+        moveTo(coordinates.first().x, coordinates.first().y)
+        for (i in 0 until coordinates.size - 1) {
+          lineTo(controlPoints1[i].x, controlPoints1[i].y)
+        }
+      }
+
+
+      /** filling the area under the path */
+      val fillPath2: Path = android.graphics.Path(dottedStroke.asAndroidPath())
+        .asComposePath().apply {
+          lineTo(xAxisSpace * xValues.last(), size.height - yAxisSpace + 100)
+          lineTo(xAxisSpace, size.height - yAxisSpace + 300)
+          close()
+        }
+
+      drawPath(
+        fillPath,
+        brush = Brush.verticalGradient(
+          listOf(Color.Cyan, Color.Transparent),
+          endY = size.height - yAxisSpace
+        ),
+      )
+      drawPath(
+        fillPath2,
+        brush = Brush.verticalGradient(
+          listOf(Color.Black, Color.Transparent),
+          endY = size.height - yAxisSpace
+        ),
+      )
+      drawLine(
+        color = Color.Red,
+        start = Offset(0f, 0f),
+        end = Offset(size.width, 0f),
+        pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+
+      )
+      drawPath(
+        stroke, color = Color.Black, style = Stroke(width = 5f, cap = StrokeCap.Square,
+          //curved
+          pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f))
+      )
     }
+  }
 }
